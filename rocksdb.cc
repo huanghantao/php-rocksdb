9b5de22b0c0421e6911b4a20133d152a7b808082
codinghuang <2812240764@qq.com>
2020-04-20 22:44:02 +0800

    feat: support more read and write options

--- rocksdb.cc
+++ rocksdb.cc
@@ -118,13 +118,25 @@ static void check_rocksdb_write_options(WriteOptions &wop, HashTable *vht)
 {
     zval *ztmp;
 
-    if (php_rocksdb_array_get_value(vht, "sync", ztmp))
-    {
-        wop.sync = zval_is_true(ztmp);
-    }
     if (php_rocksdb_array_get_value(vht, "disableWAL", ztmp))
     {
         wop.disableWAL = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "ignore_missing_column_families", ztmp))
+    {
+        wop.ignore_missing_column_families = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "low_pri", ztmp))
+    {
+        wop.low_pri = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "no_slowdown", ztmp))
+    {
+        wop.no_slowdown = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "sync", ztmp))
+    {
+        wop.sync = zval_is_true(ztmp);
     }
 }
 
@@ -132,13 +144,41 @@ static void check_rocksdb_read_options(ReadOptions &rop, HashTable *vht)
 {
     zval *ztmp;
 
-    if (php_rocksdb_array_get_value(vht, "verify_checksums", ztmp))
+    if (php_rocksdb_array_get_value(vht, "background_purge_on_iterator_cleanup", ztmp))
     {
-        rop.verify_checksums = zval_is_true(ztmp);
+        rop.background_purge_on_iterator_cleanup = zval_is_true(ztmp);
     }
     if (php_rocksdb_array_get_value(vht, "fill_cache", ztmp))
     {
         rop.fill_cache = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "ignore_range_deletions", ztmp))
+    {
+        rop.ignore_range_deletions = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "managed", ztmp))
+    {
+        rop.managed = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "pin_data", ztmp))
+    {
+        rop.pin_data = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "prefix_same_as_start", ztmp))
+    {
+        rop.prefix_same_as_start = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "tailing", ztmp))
+    {
+        rop.tailing = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "total_order_seek", ztmp))
+    {
+        rop.total_order_seek = zval_is_true(ztmp);
+    }
+    if (php_rocksdb_array_get_value(vht, "verify_checksums", ztmp))
+    {
+        rop.verify_checksums = zval_is_true(ztmp);
     }
 }
 
